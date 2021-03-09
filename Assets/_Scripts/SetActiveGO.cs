fb1506482465ddcffa1dc817d4692c4898b45f64
stephan <silent97vvaaavava@gmail.com>
2021-03-09 22:48:13 +0300

    Плавная анимация

---
+++ Assets/_Scripts/SetActiveGO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveGO : MonoBehaviour
+{
+    void SetActive()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
