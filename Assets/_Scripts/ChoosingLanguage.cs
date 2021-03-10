d858b9869830fd796f7ac7222778b6abd95a1924
stephan <silent97vvaaavava@gmail.com>
2021-03-10 18:48:44 +0300

    Перервод главного меню

---
+++ Assets/_Scripts/ChoosingLanguage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.SimpleLocalization
+{
+    public class ChoosingLanguage : MonoBehaviour
+    {
+        [SerializeField] string[] setLanguage;
+
+        private int number = 0;
+
+        public void Awake()
+        {
+            LocalizationManager.Read();
+
+            switch (Application.systemLanguage)
+            {
+               
+                case SystemLanguage.Russian:
+                    LocalizationManager.Language = "Russian";
+                    break;
+                default:
+                    LocalizationManager.Language = "English";
+                    break;
+            }
+        }
+
+        public void SetLocalization()
+        {
+            LocalizationManager.Language = setLanguage[number];
+            if(number >= setLanguage.Length - 1)
+            {
+                number = 0;
+            } else
+            {
+                number++;
+            }
+        }
+
+    }
+}
