0cb33a3b647d4cb1e68cf68b35be1b77dd40d327
stephan <silent97vvaaavava@gmail.com>
2021-03-11 15:03:19 +0300

    Исправлена локализация

---
+++ Assets/_Scripts/ChangeLocalization.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class ChangeLocalization : MonoBehaviour
+{
+    AsyncOperationHandle m_InitializeOperation;
+    List<UnityEngine.Localization.Locale> locales = new List<UnityEngine.Localization.Locale>();
+
+    int number;
+
+    private void Start()
+    {
+        m_InitializeOperation = LocalizationSettings.SelectedLocaleAsync;
+        if (m_InitializeOperation.IsDone)
+        {
+            InitializeCompleted(m_InitializeOperation);
+        }
+        else
+        {
+            m_InitializeOperation.Completed += InitializeCompleted;
+        }
+
+    }
+
+    void InitializeCompleted(AsyncOperationHandle obj)
+    {
+        locales = LocalizationSettings.AvailableLocales.Locales;
+
+        for (int i = 0; i < locales.Count; ++i)
+        {
+            var locale = locales[i];
+            Debug.Log(locales[i].name);
+        }
+    }
+
+    public void ChangeLanguage()
+    {
+        LocalizationSettings.SelectedLocale = locales[number];
+        PlayerPrefs.SetString("Language", locales[number].name);
+        if (number >= locales.Count - 1)
+        {
+            number = 0;
+        }else
+        {
+            number++;
+        }
+
+        PlayerPrefs.SetInt("Number", number);
+    }
+}
